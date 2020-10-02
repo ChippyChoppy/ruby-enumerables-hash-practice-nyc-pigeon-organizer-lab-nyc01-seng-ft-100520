fbce76254bd36879e684d10ca45d38bce99fdcc9
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 03:30:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  hashyhash = data.each_with_object({}) do |(key, value), pidge_id|
+  results = data.each_with_object({}) do |(key, value), pidge_id|
     value.collect do |key_2, value_2|
       value_2.collect do |name|
         if !pidge_id[name]
